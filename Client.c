5b215bb51aff151926a151682cb7f42c13fa8150
Brittany Arthur <brittany_arthur@hotmail.com>
2015-02-16 13:02:08 -0800

    clean up, comment, small fix

--- Client.c
+++ Client.c
@@ -19,28 +19,26 @@ To Run: gcc Client.c -o client
 
 int OpenSocket();
 
-typedef struct Socket{
-   int port;
-}Socket;
-
 int main( int argc, char * argv[] )
 {
    int port = 30001;
    printf( "creating socket on port %d\n", port );
-   Socket* socket = malloc(sizeof(struct Socket));
-   free(socket);
-
-      
+   OpenSocket(port); 
 }
 
 int OpenSocket(int port)
 {   
    struct sockaddr_in mysocket;
    //create socket
-   int open_result = socket( AF_INET, SOCK_DGRAM, IPPROTO_TCP );
+   //INFO: SOCK_DGRAM is used for UDP packets, SOCK_STREAM for TCP.
+   //INFO: AF_INET refers to addresses from the internet, IP addresses specifically. PF_INET refers to anything in the protocol, usually sockets/ports.
+   //...Passing PF_NET ensures that the connection works right. AF = Address Family. PF = Protocol Family.
+   //http://stackoverflow.com/questions/1593946/what-is-af-inet-and-why-do-i-need-it
+   int open_result = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
    if(open_result == -1)
    {
    	  printf("Could not create socket.");
+        return -1;
    }
 
    //bind socket to port
@@ -48,11 +46,11 @@ int OpenSocket(int port)
    mysocket.sin_family = AF_INET;
    mysocket.sin_port = htons( port ); //connect it to the port
 
-   if ( bind( open_result, (struct sockaddr*) &mysocket, sizeof(mysocket) ) < 0 )
-         {
-            printf( "failed to bind socket\n" );
-            return -1;
-         }
+   if (bind( open_result, (struct sockaddr*) &mysocket, sizeof(mysocket) ) < 0 )
+   {
+         printf( "Failed to bind socket.\n" );
+         return -1;
+   }
 
    return 1;
 }
