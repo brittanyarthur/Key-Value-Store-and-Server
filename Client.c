d316c7cdbd50fd546fc4745f6e635587ce1f5e21
Brittany Arthur <brittany_arthur@hotmail.com>
2015-02-16 14:20:52 -0800

    not working, but progress is made

--- Client.c
+++ Client.c
@@ -19,11 +19,12 @@ To Run: gcc Client.c -o client
 #include <arpa/inet.h> //inet_addr
 #include <unistd.h> //sleep
 
-int OpenSocket();
+int OpenSocket(int port);
+int RecieveData(int sock_fd);
 
 int main( int argc, char * argv[] )
 {
-   int port = 30001;
+   int port = 30000;
    printf( "creating socket on port %d\n", port );
    int sock_fd = OpenSocket(port); 
    if(sock_fd != -1){
@@ -33,7 +34,7 @@ int main( int argc, char * argv[] )
 
 int OpenSocket(int port)
 {   
-   struct sockaddr_in mysocket; 
+   struct sockaddr_in remote_server; 
    //create socket
    //INFO: SOCK_DGRAM is used for UDP packets, SOCK_STREAM for TCP.
    //INFO: AF_INET refers to addresses from the internet, IP addresses specifically. PF_INET refers to anything in the protocol, usually sockets/ports.
@@ -47,17 +48,17 @@ int OpenSocket(int port)
    }
 
    //bind socket to port
-   mysocket.sin_addr.s_addr = inet_addr("127.0.0.1"); //socket binds to localhost
-   mysocket.sin_family = AF_INET;
-   mysocket.sin_port = htons( port ); //connect it to the port
+   remote_server.sin_addr.s_addr = inet_addr("127.0.0.1"); //get data from this server
+   remote_server.sin_family = AF_INET;
+   remote_server.sin_port = htons( port ); //connect it to the port
 
-   if (bind( sock_fd, (struct sockaddr*) &mysocket, sizeof(mysocket) ) < 0 )
+   if (connect( sock_fd, (struct sockaddr*) &remote_server, sizeof(remote_server) ) < 0 )
    {
-         printf( "Failed to bind socket.\n" );
+         printf( "Failed to connect to remote server.\n" );
          return -1;
    }
    //what condition to break loop?
-   //???close(mysocket); how to close?
+   //???close(remote_server); how to close?
 
    return sock_fd;
 }
--- Server.c
+++ Server.c
@@ -1,43 +1,72 @@
 //Server.c
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
+#include <stdio.h>
+#include <string.h>    //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
  
+
+int OpenSocket(int port);
+int SendData(int sock_fd);
+
+typedef struct sockaddr_in sockaddr_in;
+
 int main(int argc , char *argv[])
 {
-    int socket_desc;
-    struct sockaddr_in server;
-    char *message;
-     
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
+    // create socket
+	int port = 30001;
+	printf( "creating socket on port %d\n", port );
+    int sock_fd = OpenSocket(port); 
+    if(sock_fd != -1){
+       printf("Connected\n");
+       //Send some data
+       SendData(sock_fd);
     }
-         
-    server.sin_addr.s_addr = inet_addr("74.125.235.20");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 80 );
- 
-    //Connect to remote server
-    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        puts("connect error");
-        return 1;
-    }
-     
-    puts("Connected\n");
-     
-    //Send some data
-    message = "GET / HTTP/1.1\r\n\r\n";
-    if( send(socket_desc , message , strlen(message) , 0) < 0)
-    {
-        puts("Send failed");
-        return 1;
-    }
-    puts("Data Send\n");
      
     return 0;
 }
+
+int OpenSocket(int port)
+{   
+   struct sockaddr_in mysocket; 
+   //create socket
+   //INFO: SOCK_DGRAM is used for UDP packets, SOCK_STREAM for TCP.
+   //INFO: AF_INET refers to addresses from the internet, IP addresses specifically. PF_INET refers to anything in the protocol, usually sockets/ports.
+   //...Passing PF_NET ensures that the connection works right. AF = Address Family. PF = Protocol Family.
+   //http://stackoverflow.com/questions/1593946/what-is-af-inet-and-why-do-i-need-it
+   int sock_fd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP ); //socket == file descriptor, thus: mysocket
+   if(sock_fd == -1)
+   {
+   	  printf("Could not create socket.");
+        return -1;
+   }
+   
+   mysocket.sin_addr.s_addr = inet_addr("127.0.0.1"); //socket binds to localhost
+   mysocket.sin_family = AF_INET;
+   mysocket.sin_port = htons( port ); //connect it to the port
+
+   //bind this remote server socket to port
+   if (bind( sock_fd, (struct sockaddr*) &mysocket, sizeof(mysocket) ) < 0 )
+   {
+      printf( "Failed to bind socket.\n" );
+      return -1;
+   }
+
+   return sock_fd;
+}
+
+int SendData(int sock_fd)
+{
+   char* message = "Today is BRIGHT.";
+   sockaddr_in address;
+   address.sin_family = AF_INET; //http://stackoverflow.com/questions/1593946/what-is-af-inet-and-why-do-i-need-it
+   address.sin_addr.s_addr = inet_addr("127.0.0.1");
+   address.sin_port = htons(30000);
+   while (1){
+      if(sendto(sock_fd, message, sizeof(message), 0, (struct sockaddr*)&address, sizeof(sockaddr_in)) < 0)
+      {
+          printf("Send failed\n");
+      }
+      printf("Data Sent\n");
+   }
+}
+
