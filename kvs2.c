91e516e26d8f2e5dfd22425ae53296de584fd896
Peter Froud <pfroud@ucsc.edu>
2015-02-22 18:40:24 -0800

    Revert "fixed length and size"
    This reverts commit c5d43da655d4910c2bd235b12b5347873bf257fd.
    
    Conflicts:
    	kvs2.c
    	kvs2.h

---
+++ kvs2.c
@@ -0,0 +1,41 @@
+//kvs2.c
+#include <stdio.h>
+#include <stdlib.h> //malloc()
+#include <unistd.h>
+#include "kvs2.h"
+
+//size = # of entires, length = # size of each thing
+FILE* initialize(char* name, int size, int length){
+	FILE* store;
+	if( access(name, W_OK ) != -1 )
+		store = access_file(name); //file exists
+	else
+		store = create_file(name, size, length); //file does not exist
+	return store;
+}
+
+FILE* create_file(char* name, int size, int length){
+	FILE* store = fopen(name, "w+");
+	//fill hash with fake data
+	populate(store, size, length);
+	return store;
+}
+
+FILE* access_file(char* name){
+	FILE* store;
+	store = fopen(name, "w");
+	return store;
+}
+
+void populate(FILE* store, int size, int length){
+	//The argument size will be the number of entries in your hash table
+	void* value = malloc(length);
+	fwrite(value, sizeof(value), size, store);
+}
+
+int main(){
+	int size = 10;
+	int length = 50;
+	//initialize hash table into file.
+	initialize("hashtable", size, length);
+}
---
+++ kvs2.h
@@ -0,0 +1,4 @@
+FILE* initialize(char* name, int size, int length);
+FILE* create_file(char* name, int size, int length);
+FILE* access_file(char* name);
+void populate(FILE* store, int size, int length);
