047d046b0beb20b3fe8d443ef6d2df6df5d0b2cc
Brittany Arthur <brittany_arthur@hotmail.com>
2015-02-22 19:28:14 -0800

    fixed file inserting a bit

--- kvs2.c
+++ kvs2.c
@@ -19,17 +19,19 @@ FILE* initialize(char* name){
 
 int insert(FILE* store, char* key, void* value, int length){
 	int index = hash(key)%table_size;
+	printf("value to insert is: %s\n",value);
 	fseek(store, index*table_length, SEEK_SET);
 	fwrite(value, sizeof(value), 1, store);
 	return 0;
 }
 
 int h_read(FILE* store, char* key, int length){
-	char buffer[20];
+	char buffer[table_length];
 	int index = hash(key)%table_size;
 	fseek(store, index*table_length, SEEK_SET);
 	fread(buffer, length, 1, store);
 	printf("%s\n",buffer);
+	fclose(store);
 	return 0;
 }
 
@@ -42,23 +44,29 @@ FILE* create_file(char* name){
 
 FILE* access_file(char* name){
 	FILE* store;
-	store = fopen(name, "w");
+	store = fopen(name, "a+");
 	return store;
 }
 
 void populate(FILE* store){
 	//The argument size will be the number of entries in your hash table
 	void* value = malloc(table_length);
-	fwrite(value, sizeof(value), table_size, store);
+	//fwrite(value, sizeof(value), table_size, store);
+	fwrite(value, sizeof(value), table_size*8, store);
 }
 
 int main(){
 	//initialize hash table into file.
 	FILE* store = initialize("hashtable");
-	char* val = "Jason Heron!!!";
-	insert(store, "name", val, sizeof(val));
-	h_read(store,"name", sizeof(val));
+	char* val = "Brit";
+	insert(store, "nameb", val, sizeof(val));
+	h_read(store,"nameb", sizeof(val));
 }
+
+
+
+
+
 
 
 
