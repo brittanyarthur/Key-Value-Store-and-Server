894bb7b6d6f20ce49771a1071a1472ae59f5f2b7
Brittany Arthur <brittany_arthur@hotmail.com>
2015-02-22 22:27:42 -0800

    fetch improvements

--- kvs2.c
+++ kvs2.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h> //malloc()
 #include <unistd.h>
+#include <string.h>
 #include "kvs2.h"
 
 #define table_size 360
 #define table_length 512
 	#define key_size 25
 	#define value_size 75
+const int MAGIC_NUM = 0xdeadd00d;
 
 struct kvpair{
 	char* key;
@@ -41,32 +43,49 @@ int insert(FILE* store, char* key, void* value, int length){
 	int index = hash(key)%table_size;
 	fseek(store, index*table_length, SEEK_SET);
 
+	int m = MAGIC_NUM;
+	int* magic = &m;
+	fwrite(magic, sizeof(MAGIC_NUM), 1, store);
 	fwrite(key, sizeof(key), 1, store);
 	fwrite(value, sizeof(value), 1, store);
 	return 0;
 }
 
+//return the slot location of where the value is stored. or else -1.
 int fetch(FILE* store, char* key, void* value, int* length){
+	(void)value;
+	(void)length;
+	int index = hash(key)%table_size;
+	fseek(store, index*table_length, SEEK_SET);
+
+	int magic_candidate = 0;
+	char buffer[table_length];
+	do{
+	    fread(&magic_candidate, sizeof(int)*4, 1, store);
+	    printf("Magic number is %d\n",MAGIC_NUM);
+		printf("Magic number could be %d\n", magic_candidate);
+	    if(magic_candidate == MAGIC_NUM){
+	    	printf("found magic num\n");
+	    	//check if this key is equal to the one I have
+		    	return index;
+	    }
+	    fseek(store, (++index)*table_length, SEEK_SET);
+	}while(magic_candidate != MAGIC_NUM);
+
 	//int index = probe(key);
 	return 0;
-}
-
-//continue point: Assume this works. Assume structures put null ptrs at end.
-void populate(FILE* store){
-	//The argument size will be the number of entries in your hash table
-	//fwrite(value, sizeof(value), table_size, store);
-	char filler = 0;
-	int multiply = table_length/sizeof(char);
-	fwrite(&filler, sizeof(char)*multiply, table_size, store);
 }
 
 int main(){
 	//initialize hash table into file.
 	FILE* store = initialize("hashtable");
 	char* val = "Brit"; //sample value for testing.
-
+	int val_len = sizeof(val);
+	int* val_ptr = &val_len;
 	insert(store, "nameb", val, sizeof(val));
-	h_read(store,"nameb", sizeof(val));
+	//h_read(store,"nameb", sizeof(val));
+	fetch(store, "nameb", val, val_ptr);
+	fclose(store);
 }
 
 
@@ -85,7 +104,14 @@ FILE* access_file(char* name){
 	return store;
 }
 
-
+//continue point: Assume this works. Assume structures put null ptrs at end.
+void populate(FILE* store){
+	//The argument size will be the number of entries in your hash table
+	//fwrite(value, sizeof(value), table_size, store);
+	char filler = 0;
+	int multiply = table_length/sizeof(char);
+	fwrite(&filler, sizeof(char)*multiply, table_size, store);
+}
 
 
 
@@ -102,13 +128,17 @@ int fetch_probe(char* key){
 int h_read(FILE* store, char* key, int length){
 	char buffer[table_length];
 	int index = hash(key)%table_size;
-	fseek(store, index*table_length, SEEK_SET);
-	fread(buffer, length, 1, store);
-	printf("%s\n",buffer);
+	fseek(store, (index*table_length)+4, SEEK_SET);
 
 	fread(buffer, length, 1, store);
-	printf("%s\n",buffer);
-	fclose(store);
+	printf("1 %s\n",buffer);
+
+	fread(buffer, length, 1, store);
+	printf("2 %s\n",buffer);
+/*
+	fread(buffer, length, 1, store);
+	printf("3 %s\n",buffer);*/
+	
 	return 0;
 }
 
