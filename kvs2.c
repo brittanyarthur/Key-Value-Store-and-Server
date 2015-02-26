8f6b49a47c9a1428bb613b231a0a6140bf34a395
Peter Froud <pfroud@ucsc.edu>
2015-02-25 23:04:50 -0800

    Need this change.

--- kvs2.c
+++ kvs2.c
@@ -11,6 +11,20 @@ const int TOMBSTONE = 0xdeadd00d;
 const int VALID     = 0xad00000b;
 const int INVALID   = 0xda00000b;
 
+
+FILE* initialize(char* name);
+FILE* create_file(char* name);
+FILE* access_file(char* name);
+int fetch(FILE* store, void* result, char* key, int* length);
+void read_int_array(FILE* store, char* key, int length);
+void populate(FILE* store);
+int insert(FILE* store, char* key, void* value, int length);
+void read_char_array(FILE* store, char* key, int length);
+int fetch_probe(FILE* store, char* key);
+int insert_probe(FILE* store, char* key);
+int delete(char* key);
+unsigned long hash(char *str);
+
 //pass
 FILE* initialize(char* name){
 	FILE* store;
