f77a48ef57383871eaa8343490af2df1c2f6da03
Jason Heron <JasonAHeron@Gmail.com>
2015-02-27 14:12:47 -0800

    Introduced INFLOOP but server and client now love eachother

--- Server.c
+++ Server.c
@@ -12,11 +12,11 @@ int OpenSocket(int port);
 int ListenIncomingConnection(int sock_fd);
 int AcceptConnections(int sock_fd);
 char* RecieveData(int newSocket);
-int SendData(int sock_fd, int newSocket, int data_recieved);
-int parse_client_data(char* reply_buffer);
+int SendData(int sock_fd, int newSocket, char* data_recieved);
+char* parse_client_data(char* reply_buffer);
 int do_init(char* name, char* length, char* size);
-int do_insert(char* key, char* value);
-int do_lookup(char* key);
+char* do_insert(char* key, char* value);
+char* do_lookup(char* key);
 int do_delete(char* key);
 
 #define QUIT       1
@@ -107,29 +107,27 @@ char* RecieveData(int newSocket){
     }
 }
 
-int parse_client_data(char* reply_buffer){
+char* parse_client_data(char* reply_buffer){
    char* command = malloc(sizeof(char)*100);
    char* name = malloc(sizeof(char)*100);
    char* length = malloc(sizeof(char)*100);
    char* size = malloc(sizeof(char)*100);
    char* key = malloc(sizeof(char)*100);
    char* value = malloc(sizeof(char)*100); 
-   int status = 0;
 
    sscanf(reply_buffer, "<cmd>%[^<]</cmd><name>%[^<]</name><length>%[^<]</length><size>%[^<]</size><key>%[^<]</key><value>%[^<]</value>",
     command, name, length,size, key, value);
    printf("command=\"%s\"\nname=\"%s\"\nlength=\"%s\"\nsize=\"%s\"\nkey=\"%s\"\nvalue=\"%s\"\n", command, name, length, size, key, value);
 
    if(!strcmp(command, "init")){
-      status = do_init(name,length,size);
+      return do_init(name,length,size);
    }else if(!strcmp(command, "insert")){
-      status = do_insert(key,value);
+      return do_insert(key,value);
    }else if(!strcmp(command, "delete")){
-      status = do_delete(key);
+      return do_delete(key);
    }else if(!strcmp(command, "lookup")){
-      status = do_lookup(key);
+      return do_lookup(key);
    }
-   return status;
 }
 
 int do_init(char* name, char* length, char* size){
@@ -137,25 +135,34 @@ int do_init(char* name, char* length, char* size){
   return 0;
 }
 
-int do_insert(char* key, char* value){
+char* do_insert(char* key, char* value){
   printf("inserting %s, with %s\n",key,value);
   FILE* my_data = initialize("hashtable");
   int value_size = (strlen(value) + 1)*sizeof(char);
   insert(my_data, key, value, value_size);
+  char result[max_value_size];
+  int length;
+  int * len = &length;
+  fetch(my_data, result, key, len);
   fclose(my_data);
-  return 0;
+  if(!strcmp(result, value)){
+    //insert success
+    return "INSERT_SUCCESS";
+  }
+  //insert failure
+  return "INSERT FAILURE";
 }
 
-int do_lookup(char* key){
+char* do_lookup(char* key){
   printf("look up %s\n",key);
   FILE* my_data = initialize("hashtable");
-  char result[max_value_size];
+  char* result = malloc(max_value_size*sizeof(char));
   int length;
   int* len = &length;
   fetch(my_data, result, key, len);
   printf("FOUND: %s\n", result);
   fclose(my_data);
-  return 0;
+  return result;
 }
 
 int do_delete(char* key){
@@ -186,7 +193,7 @@ int AcceptConnections(int sock_fd){
                return QUIT;
             }
             // 0 maps to other, 1 maps to no, 2 maps to yes
-            int status = parse_client_data(data_recieved); //we can change the return value to a char* but then we would have to allocate memory
+            char* status = parse_client_data(data_recieved); //we can change the return value to a char* but then we would have to allocate memory
             SendData(sock_fd, newSocket, status);
          }
          return 0;
@@ -197,19 +204,11 @@ int AcceptConnections(int sock_fd){
     }
 }
 
-int SendData(int sock_fd, int newSocket, int data_recieved)
+int SendData(int sock_fd, int newSocket, char* data_recieved)
 {
     (void)sock_fd;
     //Finally, a message can be sent!
-    char buffer[256];
-    if(data_recieved == 1){
-       strcpy(buffer,"It must be a study day for you then!"); 
-    }else if(data_recieved == 2){
-       strcpy(buffer,"Today is BRIGHT."); 
-    }else{
-       strcpy(buffer,"Nooo you have to answer the question!!"); 
-    }
-    if(send(newSocket,buffer,sizeof(buffer),0) < 0){
+    if(send(newSocket,data_recieved,sizeof(data_recieved),0) < 0){
     	printf("Error sending message\n");
     	return -1;
     }
