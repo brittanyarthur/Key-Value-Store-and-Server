e07f2ffae02263048e7874ed7aac18dc1ec8d841
Jason Heron <JasonAHeron@Gmail.com>
2015-02-27 17:22:48 -0800

    Revert "cherry picked commit 65214c704f9"
    This reverts commit 789cf3dfe45a55ae842e3ed91fb4a5de998133ca.

--- Client.c
+++ Client.c
@@ -19,12 +19,13 @@ char* do_quit();
 int parse_command_response(char* response);
 //char* BuildPacket(char* cmd, char* name, char* length, char* size, char* key, char* value);
 
-int main( int argc, char * argv[] ) {
+int main( int argc, char * argv[] )
+{
    (void)argc;
    (void)argv;
    int remote_port =  10732;
    const char* remote_IP = "127.0.0.1";
-   int sock_fd = OpenSocket(remote_port, remote_IP);
+   int sock_fd = OpenSocket(remote_port, remote_IP); 
    printf("Socket has been opened\n");
    int write_result = 0;
    int recieve_result = 0;
@@ -37,23 +38,21 @@ int main( int argc, char * argv[] ) {
             return 0;
          }
       }else{
-         printf("Error: file descriptor error\n");
-         return EXIT_FAILURE;
+         printf("Error: file descriptor error");
       }
       if(write_result != -1){
          recieve_result = RecieveData(sock_fd);
       }
       if(recieve_result == -1){
          printf("An Error has occured.\n");
-         return EXIT_FAILURE;
       }
    }
    return 0;
 }
 
 int OpenSocket(int port, const char* remote_IP)
-{
-   struct sockaddr_in remote_server;
+{   
+   struct sockaddr_in remote_server; 
    //create socket
    //INFO: SOCK_DGRAM is used for UDP packets, SOCK_STREAM for TCP.
    //INFO: AF_INET refers to addresses from the internet, IP addresses specifically. PF_INET refers to anything in the protocol, usually sockets/ports.
@@ -68,11 +67,11 @@ int OpenSocket(int port, const char* remote_IP)
 
    // Set IP address
    // inet_addr interprets character string and returns binary rep of internet address
-   remote_server.sin_addr.s_addr = inet_addr(remote_IP);
-   // Address family = Internet
+   remote_server.sin_addr.s_addr = inet_addr(remote_IP); 
+   // Address family = Internet 
    remote_server.sin_family = AF_INET;
    // Set port number. htons function creates proper bigendian byte order for networking.
-   remote_server.sin_port = htons(port);
+   remote_server.sin_port = htons(port); 
 
    //make pointer to socket info
    struct sockaddr_in* info_ptr = &remote_server;
@@ -96,9 +95,8 @@ int WriteData(int sock_fd){
    char* write_buffer;
    while(no_response){ //loop until there is a response
       printf("\n[I]nsert: add a key/value pair to keystore.\n[D]elete: delete a key/value pair from keystore.\n[L]ookup: lookup value in keystore.\n[S]etup: configure keystore.\n[Q]uit: quit: \n");
-	char* command_buffer = malloc(256*sizeof(char));
-      //char command_buffer[256];
-      //memset(command_buffer,'\0',strlen(command_buffer));
+      char command_buffer[256];
+      memset(command_buffer,'\0',strlen(command_buffer));
       fgets(command_buffer, sizeof(command_buffer), stdin);
       command_buffer[strlen(command_buffer)-1] = '\0';
       if(strcmp(command_buffer, "quit") == 0){
@@ -123,7 +121,7 @@ int WriteData(int sock_fd){
          no_response = 1;
       }
    }
-
+   
    //Send formed packet to server
    printf("writing to server: %s\n", write_buffer);
    int result_status = write(sock_fd, write_buffer, strlen(write_buffer));
@@ -165,22 +163,15 @@ char* do_quit(){
 
 char* do_insert(){
    printf("\nKey: ");
-   char* key_buffer = calloc(256, sizeof(char)); //calloc zeroes memory for you!
-   //char key_buffer[256];
-   //memset(key_buffer,'\0',strlen(key_buffer));
-
+   char key_buffer[256];
+   memset(key_buffer,'\0',strlen(key_buffer));
    fgets(key_buffer, sizeof(key_buffer), stdin);
-   //key_buffer[strlen(key_buffer)-1] = '\0';
-   strtok(key_buffer, "\n");
+   key_buffer[strlen(key_buffer)-1] = '\0';
    printf("\nValue: ");
-
-   char* value_buffer = calloc(256, sizeof(char)); //calloc zeroes memory for you!
-   //char value_buffer[256];
-   //memset(value_buffer,'\0',strlen(value_buffer));
-
+   char value_buffer[256];
+   memset(value_buffer,'\0',strlen(value_buffer));
    fgets(value_buffer, sizeof(value_buffer), stdin);
-   //value_buffer[strlen(value_buffer)-1] = '\0';
-   strtok(value_buffer, "\n");
+   value_buffer[strlen(value_buffer)-1] = '\0';
 
    char* formatter = "<cmd>insert</cmd><name>NONE</name><length>NONE</length><size>NONE</size><key></key><value></value>";
    int cmdsize = strlen(key_buffer) + strlen(value_buffer) + strlen(formatter) + 1;
@@ -244,12 +235,10 @@ char* do_init(){
    char* formatter = "<cmd>init</cmd><name></name><length></length><size></size><key>NONE</key><value>NONE</value>";
    int cmdsize = strlen(name_buffer) + strlen(length_buffer) + strlen(size_buffer) + strlen(formatter) + 1;
    char* packet = malloc(cmdsize);
-
    sprintf(packet, "<cmd>init</cmd><name>%s</name><length>%s</length><size>%s</size><key>NONE</key><value>NONE</value>", name_buffer, length_buffer, size_buffer);
    printf("packet is %s\n", packet);
    return packet;
 }
-
 
 int RecieveData(int sock_fd){
    unsigned char reply_buffer[256];
